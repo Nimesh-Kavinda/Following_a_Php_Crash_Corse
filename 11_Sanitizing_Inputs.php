f10319ce68a0bdf1c36f33aa8e3db17bccd4222f
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-10-12 23:01:32 +0530

    Sanitizing Inputs (filtering inputs from script and other posibel codes)

--- 11_Sanitizing_Inputs.php
+++ 11_Sanitizing_Inputs.php
@@ -10,16 +10,19 @@
 
 
 if(isset($_POST['submit'])){
-    echo $_POST['name'];
-    echo $_POST['age'];
+    $name = filter_input(INPUT_POST,'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $age = filter_input(INPUT_POST,'age', FILTER_SANITIZE_SPECIAL_CHARS); 
+
+//  $name = filter_var($_POST['name'],FILTER_SANITIZE_SPECIAL_CHARS);
+
 };
 
-
+    echo $name;
 ?>
 
 
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
 
 <div>
 <label for="name">Name: </label>
