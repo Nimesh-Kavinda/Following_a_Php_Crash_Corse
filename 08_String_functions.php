bee0fc295f12802fddcd5c6a0863d37af9616142
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-10-12 13:50:20 +0530

    Learned String Functions (Inbuild in php)

---
+++ 08_String_functions.php
@@ -0,0 +1,63 @@
+<?php
+$string = 'Hello World';
+
+// Get the Length of a String
+
+// echo strlen($string);
+
+// Find the position of the first occurrece of a Substing in a Stirng
+
+// echo strpos($string, 'o');
+
+// Find the posotion of the last occurrence of a substring in a String
+
+// echo strrpos($string, 'o');
+
+// Reverse A Srting
+
+// echo strrev($string);
+
+// Convert All Caracters to lowerCase
+
+// echo strtolower($string);
+
+// Convert All Caracters to uppercase
+
+// echo strtoupper($string);
+
+// UpperCse to first character of a each word
+
+    // echo ucwords($string);
+
+// String Replace 
+
+//  echo str_replace('World', 'EveryOne', $string);
+
+// Return the position of a String specified by the offset and lenght
+
+    // echo substr($string, 0, 5);
+    // echo substr($string, 5);
+
+// Starts with 
+
+// if(str_starts_with($string, 'Hello')){
+//     echo 'yes';
+// }
+
+
+// Ends With 
+
+// if(str_ends_with($string, 'ld')){
+//     echo 'Yes';
+// }
+
+
+    // $string2 = '<script>alert(1)</script>';
+    // echo htmlspecialchars($string2); // echo in Html Format
+
+    // printf('%s likes to %s', 'Nimesh', 'Code'); // %s Get the given values
+    // printf('1+1 = %d', 1+1);
+
+
+
+?>
