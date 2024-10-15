f59ec9c0823edc5c6b738d2133e6472d0ebd292c
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-10-15 10:16:15 +0530

    Done Database and Feedback pages Structure to start a simple feedback project

--- 17_OOP.php
+++ 17_OOP.php
@@ -1,11 +1,11 @@
 <?php
 
-/* ----------- Object Orientad Programming ---------- */
+/* ----------- Object-Oriented Programming ---------- */
 
 /* 
-    From PHPS onwards you can wirte PHP in either a procedural or
-    object oriented way. OOP consist of classes that can hold "Properties" 
-    and "methods" . Objects can be created from classes.
+    From PHPS onwards you can write PHP in either a procedural or
+    object-oriented way. OOP consists of classes that can hold "Properties" 
+    and "methods". Objects can be created from classes.
 */
 
 
@@ -15,47 +15,46 @@ class User {
     public $email;
     public $password;
 
-
-
-    // A constructor is a method rhat runs an object is created
-
+    // A constructor is a method that runs when an object is created
     public function __construct($name, $email, $password){
        $this -> name = $name;
        $this -> email = $email;
        $this -> password = $password;
     }
 
-    // Method is a functin that belongs to a class
-
+    // Method is a function that belongs to a class
     function set_name ($name){
-        $this->name = $name;
+        $this -> name = $name;
     }
 
     function get_name() {
-        return $this->name;
+        return $this -> name;
     }
 }
 
 // Instantiate a User Object
-
 $user1 = new User('Nimesh', 'mmm@gmail.com', '123');
 $user2 = new User('Kavinda', 'kavi@gmail.com', '234');
 
 // $user1-> set_name('Kavinda');
 // $user2-> set_name('Nimesh');
 
-echo $user1 -> email;
-echo $user2 -> name;
+// echo $user1 -> email;
+// echo $user2 -> name;
 
 // echo $user1 -> get_name();
 // echo $user2 -> get_name();
 
 // Inheritance 
-
 class Employee extends User {
+    // Declare the title property
+    public $title;
+
     public function __construct($name, $email, $password, $title)
     {
+        // Call the parent constructor
         parent::__construct($name, $email, $password);
+        // Assign the title
         $this -> title = $title;
     }
 
@@ -64,9 +63,8 @@ class Employee extends User {
     }
 }
 
-
-
-
-
+// Instantiate an Employee Object
+$employee1 = new Employee('Ravi', 'ravi@gmail.com', '456', 'Manager');
+echo $employee1->get_title();
 
 ?>
